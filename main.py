ad1ffa4c172560f254b611c0309602e8d781a6a9
Def <def@mailo.com>
2022-05-05 12:56:29 +0200

    retrieve IDs of feed segments

--- main.py
+++ main.py
@@ -23,6 +23,9 @@ from bs4 import BeautifulSoup
 from lxml import etree
 
 
+MTVN_EPISODE_DATA_URL = "https://media.mtvnservices.com/pmt/e1/access/index.html?uri=mgid:arc:episode:southpark.intl:{}&configtype=edge&ref={}"
+
+
 def download_episode(ep_url: str):
     response = requests.get(ep_url)
 
@@ -31,7 +34,14 @@ def download_episode(ep_url: str):
     dom = etree.HTML(str(soup))
     
     episode_data = json.loads(dom.xpath('/html/body/div[1]/main/div/div[1]/script')[0].text)
-
+    
+    cdn_episode_data = requests.get(MTVN_EPISODE_DATA_URL.format(episode_data['@id'], ep_url)).json()
+    
+    # The South Park video player splits the video into ad-seperated segments,
+    # so we have to fetch all of them in order to have the whole episode.
+    video_feed_ids = []
+    for feed in cdn_episode_data['feed']['items']:
+        video_feed_ids.append(feed['group']['category']['id'])
 
 def download_season():
     pass
